96ae0f02afe3840c84d72481926af0caef8c28a7
BenjCaro <carbenjamin@gmail.com>
2025-06-28 18:45:29 +0200

    Ajout insert() dans BaseModel pour ajouter des données à la BDD, + test de celle ci avec PhPUnit

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -7,16 +7,14 @@ use PDO;
 class BaseModel {
 
     protected PDO $pdo;
+    protected int $id;
     protected string $table;
 
- 
-
-    public function __construct(PDO $pdo)
-    {
+    public function __construct(PDO $pdo) {
         $this->pdo = $pdo;
     }
 
-    public function getById(int $id) {
+    public function findById(int $id) {
         $stmt = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE id = :id");
         $stmt->execute(['id' => $id]);
         return $stmt->fetch(PDO::FETCH_ASSOC) ?: null;
@@ -29,9 +27,13 @@ class BaseModel {
          return $stmt->fetch(PDO::FETCH_ASSOC) ?: null;
     }
 
-    public function insert() {
-        // "INSERT INTO {$this->table} " insert() pour insérer des données dynamiques 
-        // passer d'un tableau à une chaine 
+    public function insert(array $data) {
+        $array = array_keys($data);
+        $columns = implode( ", " , $array);
+        $values = ':' . implode(', :', $array);
+        
+        $stmt = $this->pdo->prepare("INSERT INTO {$this->table} ($columns) VALUES ($values)"); 
+        $stmt->execute($data);
 
     }   
     
