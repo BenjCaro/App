9763ba6e4ab241e7947b8f173ea99f3e73b1d8dc
BenjCaro <carbenjamin@gmail.com>
2025-06-29 13:56:46 +0200

    creation getters et setters UserModel

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -13,8 +13,43 @@ class UserModel extends BaseModel {
    private string $email;
    private string $password;
    private string $role;
-   private  $createAt; 
+   private string $createAt; 
 
   // mettre en place un construct et hydrate() (à dev dans le BaseModel)
 
+  public function getName() :string {
+    return $this->name;
+  }
+  
+  public function setName(string $name) :void  {
+     $this->name = $name;
+  }
+
+  public function getFirstname() :string {
+    return $this->firstname;
+  }
+
+  public function setFirstname(string $firstname) :void {
+    $this->firstname = $firstname;
+  }
+
+  public function getEmail() :string {
+     return $this->email;
+  }
+
+  public function setEmail(string $email) :void {
+    $this->email = $email;
+  }
+
+  public function setPassword(string $password) :void {
+    $this->password = password_hash($password, PASSWORD_DEFAULT); 
+  }
+
+  public function getRole(string $role) :string {
+    return $this->role;
+  }
+
+  public function getCreateAt() :string {
+    return $this->createAt;
+  }
 }
