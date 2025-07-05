1796c17b173152b4c6787cb326deb4bad47cdc1a
BenjCaro <carbenjamin@gmail.com>
2025-07-05 14:08:27 +0200

    feat: Add AltoRouter class for improved routing functionality and update composer.json autoload configuration

--- public/index.php
+++ public/index.php
@@ -3,13 +3,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 define('VIEW_PATH', dirname(__DIR__) . '/src/Views');
 
-use Carbe\App\Controllers\HomeController;
+// use Carbe\App\Controllers\HomeController;
 
- $home = new HomeController();
- $home->index();
+//  $home = new HomeController();
+//  $home->index();
 
 
-
+$router= new AltoRouter();
 
 
  
