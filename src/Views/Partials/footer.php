eddf7457c605929609d075d18e73f1935ebf76bb
BenjCaro <carbenjamin@gmail.com>
2025-07-07 18:17:49 +0200

    Add social media icons and update footer layout
    - Added SVG icons for Facebook, Instagram, Pinterest, and Twitter.
    - Updated footer structure to include navigation links and social media icons.
    - Enhanced styling in SCSS to accommodate new footer elements and improve text appearance.

--- src/Views/Partials/footer.php
+++ src/Views/Partials/footer.php
@@ -1,1 +1,30 @@
-<footer class="text-center bg-primary border-top border-cacao border-2">Petit Creux 2025 tous droits réservés</footer>
+<footer class="text-center bg-primary border-top border-light border-2 p-3 d-flex justify-content-evenly">
+    
+<div>
+    <ul class="list-unstyled">
+        <li class="nav-item fs-5"><a class="text-cacao nav-link" href="">Catégories</a></li>
+        <li class="nav-item fs-5"><a class="text-cacao nav-link" href="">A propos</a></li>
+        <li class="nav-item fs-5"><a class="text-cacao nav-link" href="">Contact</a></li>
+        <li class="nav-item fs-5"><a class="text-cacao nav-link" href="">Mention Légales</a></li>
+    </ul>
+</div>
+<div>
+    <ul class="list-unstyled">
+        <li class="nav-item fs-5"><a class="text-cacao nav-link" href="">Mon Compte</a></li>
+        <li class="nav-item fs-5"><a class="text-cacao nav-link" href="">Conditions générales d'utilisations</a></li>
+        <li class="nav-item fs-5"><a class="text-cacao nav-link" href="">Se déconnecter</a></li>
+    </ul>
+</div>
+ <div>
+    <h4 class="fs-5">Suivez-nous</h4>
+    <a href="" target="_blank"><img src="/assets/images/socials/instagram.svg" alt="instagram"></a>
+    <a href="" target="_blank"><img src="/assets/images/socials/twitter.svg" alt="twitter"></a>
+    <a href="" target="_blank"><img src="/assets/images/socials/facebook.svg" alt="facebook"></a>
+    <a href="" target="_blank"><img src="/assets/images/socials/pinterest.svg" alt="pinterest"></a>   
+ </div>
+</footer>
+
+
+
+
+<!-- <span>Petit Creux 2025 tous droits réservés</span> -->
