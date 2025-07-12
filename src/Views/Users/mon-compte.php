aaa405933a9c484b3e89bf8c8c6ba16d337a458e
BenjCaro <carbenjamin@gmail.com>
2025-07-12 10:08:04 +0200

    Add description field to UserModel and update account view to display it

--- src/Views/Users/mon-compte.php
+++ src/Views/Users/mon-compte.php
@@ -1,8 +1,6 @@
 <?php
 namespace Carbe\App\Views\Pages;
 
-
-
 if (!isset($_SESSION['auth_user'])) {
     $_SESSION['flash'] = "Connectez-vous pour accèder à cette page!";
     header('Location: /login');
@@ -11,20 +9,31 @@ if (!isset($_SESSION['auth_user'])) {
 ?> 
 
 <main class='container p-3 bg-light border-end border-start border-secondary'>
-    <h2>Mon Compte </h2>
-    <section>
-        <div class="card" style="width: 18rem;">
+    <h2 class="text-center">Mon Compte </h2>
+    <section class="row d-flex justify-content-center">
+        <div class="card col-6">
             <div class="card-body">
                 <h5 class="card-title">Mes informations</h5>
                 <h6 class="card-subtitle mb-2 text-body-secondary"><?= $user->getFirstname(); ?> <?= $user->getName(); ?></h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <p class="card-text"> <?= $user->getEmail(); ?>  </p>
+                <p class="card-text"><?= $user->getDescription()?></p>
+                <span class="card-text"> <?= $user->getEmail(); ?>  </span><br>
+                <span class="card-text">Membre depuis: <?= $user->getCreatedAt() ?></span>
             </div>
         </div>
+    </section>
+    <section class="row d-flex justify-content-center">
+        Gérer mes favoris
+<!-- Enlever une recette des favoris -->
 
     </section>
-    
+    <section class="row d-flex justify-content-center">
+        Mes recettes ajoutées
+<!-- Afficher les recettes ajoutées par l'utilisateur modifier/supprimer -->
 
-
+    </section>
+    <section class="row d-flex justify-content-center">
+        Mes commentaires
+<!-- Afficher les commentaires ajoutés par l'utilisateur modifier/supprimer -->
+    </section>
 </main>
 
