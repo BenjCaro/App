cd03721dc3a9bd93b90a26009f525315aa175450
BenjCaro <carbenjamin@gmail.com>
2025-07-20 10:54:35 +0200

    Ajout d'un lien pour accéder à la page d'ajout de recette depuis la page d'accueil

--- src/Views/Users/ajout-recette.php
+++ src/Views/Users/ajout-recette.php
@@ -25,22 +25,37 @@ namespace Carbe\App\Views\Users;
                 </select>
             </div>
             <div>
-                <label for="" class="form-label">Ingrédients</label>
-                <div id="">
-                    
+                <div id="ingredients">
+                    <label for="ingredient" class="form-label">Ingrédients</label>  
                 </div>
             </div>
             <div>
                 <label for="" class="form-label">Durée</label>
                 <input type="text" class="form-control">
             </div>
-            <div>
+            <div class="mb-2">
                 <label for="" class="form-label">Description</label>
                 <textarea class="form-control" name="" id="" placeholder="Décrivez les étapes de la recette"></textarea>
+            </div>
+            <div class="text-center mb-2">
+                <button class="btn btn-secondary w-25" type="submit">Valider</button>
             </div>
         </form>
     </div>
 </main>
 <script>
+    const ingredients = document.getElementById('ingredients');
+    const select = document.createElement('select');
 
+    select.name = 'ingredient';
+    select.id = 'ingredient'
+    select.classList.add('form-control');
+
+    const option = document.createElement('option');
+    const valueOption = 'choisir un ou plusieurs ingrédients';
+    
+    
+    option.textContent = valueOption
+    select.appendChild(option);
+    ingredients.appendChild(select);
 </script>
