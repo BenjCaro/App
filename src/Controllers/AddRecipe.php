0fe58ec9d80e6b13421cccd19acfdf0891746a60
BenjCaro <carbenjamin@gmail.com>
2025-07-20 15:17:46 +0200

    fonction generateSlug pour generer des slugs a partir du title

--- src/Controllers/AddRecipe.php
+++ src/Controllers/AddRecipe.php
@@ -32,4 +32,62 @@ class AddRecipe extends BaseController {
       return $ingredients;
  }
 
+ public function createRecipe(array $data) {
+     session_start();
+
+     $title = trim($data['title']);
+     $idUser = $data['id_user'];
+     $idCategory = $data['id_category'];
+     $duration = trim($data['duration']);
+     $description = trim($data['description']);
+
+     $ingredients[] = $data['ingredients'];
+     $quantity[] = $data['quantity'];
+     $unit[] = $data['unit'];
+
+     $errors = [];
+
+     if(empty($title)) {
+          $errors['title'] = "Veuillez donner un titre à la recette.";
+     }
+
+     if(empty($idUser)) {
+          $errors['id_user'] = 'Veuillez vous reconnecter à votre compte.';
+     }
+
+     if(empty($idCategory)) {
+          $errors['id_category'] = 'Sélectionner une catégorie pour votre recette.';
+     }
+
+     if(empty($duration)) {
+          $errors['duration']= "Veuillez préciser une durée de préparation pour votre recette.";
+     }
+
+     if(empty($description)) {
+          $errors['description'] = "Veuillez rédiger les étapes de préparation de votre recette.";
+     }
+
+     if(empty($ingredients || $quantity || $unit)) {
+          $errors['ingredients[]'] = "Veuillez selectionner un ingrédient, sa quantité et l'unité.";
+     }
+
+     if (!empty($errors)) {
+            $_SESSION['errors'] = $errors;
+            header('Location: /ajout-recette');
+            exit;
+     }
+
+     $slug = $this->generateSlug($title);
+
+ } 
+
+   private function generateSlug(string $string) :string {
+
+          $string = mb_strtolower($string);
+          $string = preg_replace('/[^\w\s-]/', '', $string);
+          $string = preg_replace('/[\s]+/', '-', $string);  
+          $slug = trim($string, '-');
+          return $slug;
+   }
+
 }
--- src/Views/Users/ajout-recette.php
+++ src/Views/Users/ajout-recette.php
@@ -1,33 +1,29 @@
 <?php 
 namespace Carbe\App\Views\Users;
 
-$title = "Pancakes aux yahourts grecs";
-$title = lcfirst($title);
-$slug = str_replace(' ', '-', $title);
-
 ?>
 
 
 <main class='container p-3 bg-light'>
     <div class="d-flex flex-column w-50 m-auto">
         <form action="/ajout-recette" method="post" class="form-control pb-2">
-            <input type="hidden" value="<?=$_SESSION['auth_user']['id'] ?>">
+            <input type="hidden" name="id_user" id="id_user" value="<?=$_SESSION['auth_user']['id'] ?>">
             <div class="mb-2">
                 <label for="title" class="form-label">Titre de la recette</label>
-                <input type="text" id="title" name="title" value="title" class="form-control">
+                <input type="text" id="title" name="title" class="form-control" required>
             </div>
             <div class="mb-2">
-                <label for="category" class="form-label">Catégorie</label>
-                <select name="category" id="category" class="form-select">
+                <label for="id_category" class="form-label">Catégorie</label>
+                <select name="id_category" id="id_category" class="form-select" required>
                     <option value="">Choisir la catégorie</option>
                 <?php foreach($categories as $category): ?>
                     <option value="<?= htmlspecialchars($category->getId()); ?>"><?= htmlspecialchars($category->getName()); ?></option>
                 <?php endforeach; ?>
                 </select>
             </div>
-             <div class="mb-2">
+            <div class="mb-2">
                 <label for="duration" class="form-label">Durée</label>
-                <input type="text" id="duration" name="duration" value="duration" class="form-control">
+                <input type="text" id="duration" name="duration" value="duration" class="form-control" required>
             </div>
             <div class="mb-2">
                 <label class="form-label">Sélectionner les ingrédients</label>
@@ -36,7 +32,7 @@ $slug = str_replace(' ', '-', $title);
             </div>
             <div class="mb-2">
                 <label for="description" class="form-label">Description</label>
-                <textarea class="form-control" name="description" id="description" placeholder="Décrivez les étapes de la recette"></textarea>
+                <textarea class="form-control" name="description" id="description" placeholder="Décrivez les étapes de la recette" required></textarea>
             </div>
             <div class="text-center mt-2 mb-2">
                 <button class="btn btn-secondary w-25" type="submit">Valider</button>
