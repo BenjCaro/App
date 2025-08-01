592b3de1b25e3d09ef391860c42435fef3d0f800
BenjCaro <carbenjamin@gmail.com>
2025-08-01 11:00:42 +0200

    ajout d'une div ds la page de resultat de recherche

--- src/Views/Pages/search.php
+++ src/Views/Pages/search.php
@@ -6,12 +6,14 @@ namespace Carbe\App\Views\Pages;
 <main class='container p-3 bg-light'>
     <?php if($results) { ?>
         <?php foreach($results as $result) {  ?>
-                <h3 class="card-subtitle text-body-secondary fs-4 mt-2 mb-2"> 
+                <div>
+                    <h3 class="card-subtitle text-body-secondary fs-4 mt-2 mb-2"> 
                                 <?= $result->getTitle()?>
                         </h3>
                         <span class="badge text-bg-secondary"><?= htmlspecialchars($result->getDuration()) . ' minutes' ?></span>
                         <span class="badge text-bg-secondary"><?= $result->getCategory()->getName(); ?></span> <br>
                         <button type="button" class="btn btn-secondary mt-2"><a class="text-black nav-link" href="/recette/<?= $result->getSlug()?>">Voir la recette</a></button>
+                </div>
                         <?php } ?>
         
     <?php } else { ?>
