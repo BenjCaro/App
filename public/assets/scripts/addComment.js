939ee17b0afdce68d27ab8f2aaf41c5ac6e9ca32
BenjCaro <carbenjamin@gmail.com>
2025-08-03 18:25:45 +0200

    creation addComment.js pour creation de commentaire coté interface

---
+++ public/assets/scripts/addComment.js
@@ -0,0 +1,49 @@
+const btnPost = document.getElementById('btnPost');
+    const container = document.getElementById('container');
+    btnPost.addEventListener('click', (event) => {
+
+        if (container.querySelector('form')) return; // n'affiche qu'un seul formulaire 
+       
+        const form = document.createElement('form');
+        form.classList.add('form-control', 'w-50', 'd-flex', 'flex-column', 'align-items-center', 'pb-2', 'border-gris', 'bg-gris','shadow-sm', 'p-3', 'mb-5', 'bg-body-gris');
+        form.method = "post";
+        form.action = `/recette/${slug}/commentaires`;
+
+
+        const inputUser = document.createElement('input');
+        inputUser.type = 'hidden';
+        inputUser.name = 'id_user';
+        inputUser.value = id;
+
+        const inputRecipe = document.createElement('input');
+        inputRecipe.type = 'hidden';
+        inputRecipe.name = 'id_recipe';
+        inputRecipe.value = idRecipe;
+
+        form.appendChild(inputUser);
+        form.appendChild(inputRecipe);
+
+
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.name = 'title';
+        input.required = true;
+        input.classList.add('form-control', 'mb-2');
+        input.placeholder = 'Titre de votre commentaire';
+
+        const textarea = document.createElement('textarea');
+        textarea.classList.add('form-control', 'mb-2');
+        textarea.name = 'content';
+        textarea.required = true;
+
+        const submitBtn = document.createElement('button');
+        submitBtn.classList.add('btn', 'btn-secondary');
+        submitBtn.type = 'submit';
+        submitBtn.textContent = 'Poster votre commentaire';
+
+        form.appendChild(input);
+        form.appendChild(textarea);
+        form.appendChild(submitBtn);
+        
+        container.appendChild(form);
+ });
--- src/Views/Pages/recette.php
+++ src/Views/Pages/recette.php
@@ -92,58 +92,8 @@ $steps = json_decode($recipe->getDescription(), true); // true pour avoir un tab
 </main>
 
 <script>
-
     const id = <?= json_encode($_SESSION['auth_user']['id']); ?>;
     const idRecipe = <?= json_encode($recipe->getId()); ?>;
-    
-    const btnPost = document.getElementById('btnPost');
-    const container = document.getElementById('container');
-    btnPost.addEventListener('click', (event) => {
-
-        if (container.querySelector('form')) return; // n'affiche qu'un seul formulaire 
-       
-        const form = document.createElement('form');
-        form.classList.add('form-control', 'w-50', 'd-flex', 'flex-column', 'align-items-center', 'pb-2', 'border-gris', 'bg-gris','shadow-sm', 'p-3', 'mb-5', 'bg-body-gris');
-        form.method = "post";
-        form.action = '/recette/<?= $recipe->getSlug()?>/commentaires';
-
-
-        const inputUser = document.createElement('input');
-        inputUser.type = 'hidden';
-        inputUser.name = 'id_user';
-        inputUser.value = id;
-
-        const inputRecipe = document.createElement('input');
-        inputRecipe.type = 'hidden';
-        inputRecipe.name = 'id_recipe';
-        inputRecipe.value = idRecipe;
-
-        form.appendChild(inputUser);
-        form.appendChild(inputRecipe);
-
-
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.name = 'title';
-        input.required = true;
-        input.classList.add('form-control', 'mb-2');
-        input.placeholder = 'Titre de votre commentaire';
-
-        const textarea = document.createElement('textarea');
-        textarea.classList.add('form-control', 'mb-2');
-        textarea.name = 'content';
-        textarea.required = true;
-
-        const submitBtn = document.createElement('button');
-        submitBtn.classList.add('btn', 'btn-secondary');
-        submitBtn.type = 'submit';
-        submitBtn.textContent = 'Poster votre commentaire';
-
-        form.appendChild(input);
-        form.appendChild(textarea);
-        form.appendChild(submitBtn);
-        
-        container.appendChild(form);
-
-    });
-</script>
+    const slug = <?= json_encode($recipe->getSlug())?>
+ </script>
+ <script type="text/javascript" src="/assets/scripts/addComment.js"></script>
