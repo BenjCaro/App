e0ef0e389ee1d31158b9dc4487979919b0e37026
BenjCaro <carbenjamin@gmail.com>
2025-08-05 16:36:17 +0200

    correctif : les deux formulaires sont indépendants

--- public/assets/scripts/updateUser.js
+++ public/assets/scripts/updateUser.js
@@ -1,17 +1,19 @@
 const updateBtn = document.getElementById("editInformation");
-const inputs = document.querySelectorAll('input');
+const formInformation = document.getElementById('formInformation');
+const formDescription = document.getElementById('formDescription');
+const inputs = formInformation.querySelectorAll('input');
 const btn = document.getElementById('editDescription');
 const textarea = document.getElementById('description');
-const formDescription = document.getElementById('formDescription');
-let editing = false;
-
+let editingInfos = false;
+let editingDescription = false;
 
 updateBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
-      if(!editing) {
+      if(!editingInfos) {
 
-        editing= true;
+        editingInfos= true;
+        updateBtn.textContent = "Confirmez les modifications";
         updateBtn.classList.remove("btn-primary");
         updateBtn.classList.add("btn-secondary");
         inputs.forEach(input => {
@@ -29,21 +31,35 @@ updateBtn.addEventListener('click', (event) => {
 
     formInformation.addEventListener('submit', (event) => {
             event.preventDefault(); 
-            confirm('Etes vous sur de valider vos modifications?');
-            formInformation.submit();
-            console.log('Informations soumises');
+           if(confirm('Etes vous sur de valider vos modifications?')) {
+
+                formInformation.submit();
+
+           } else {
+
+              editingInfos = false;
+              updateBtn.textContent = "Modifier ma description";
+              updateBtn.classList.replace('btn-secondary', 'btn-primary');
+              inputs.forEach(input => {
+              input.setAttribute('readonly', true);
+              input.classList.add('bg-gris');
+          
+        })
+              
+     }
+            
+            
      });
 
 
-btn.addEventListener('click', (event) => {
+    btn.addEventListener('click', (event) => {
         event.preventDefault();
 
-        if (!editing) {
+        if (!editingDescription) {
             
-            editing = true;
+            editingDescription = true;
             btn.textContent = "Confirmez les modifications";
-            btn.classList.remove("btn-primary");
-            btn.classList.add("btn-secondary");
+            btn.classList.replace('btn-primary', 'btn-secondary');
             textarea.removeAttribute('readonly');
             textarea.classList.remove('bg-gris');
             
@@ -53,10 +69,21 @@ btn.addEventListener('click', (event) => {
         }
     });
 
-    formDescription.addEventListener('submit', (event) => {
+   formDescription.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-        event.preventDefault(); 
-        confirm('Etes vous sur de valider vos modifications?');
+    if (confirm('Êtes-vous sûr de valider vos modifications ?')) {
         formDescription.submit();
-        console.log('Description soumise');
-    });
+       
+
+    } else {
+
+        editingDescription = false; 
+        btn.textContent = "Modifier ma description";
+        btn.classList.replace('btn-secondary', 'btn-primary');
+        textarea.setAttribute('readonly', true);
+        textarea.classList.add('bg-gris');
+    }
+});
+
+        
