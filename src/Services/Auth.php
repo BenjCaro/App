8f1593941bb87acac65b0bc32c70733e159992b6
BenjCaro <carbenjamin@gmail.com>
2025-08-27 08:58:40 +0200

    commentaires ajoutés

--- src/Services/Auth.php
+++ src/Services/Auth.php
@@ -5,6 +5,12 @@ use Carbe\App\Services\Flash;
 
 class Auth {
 
+    /**
+     * Ouvre une session 
+     * S'assure que l'utilisateur doit etre connecté pour accéder à certaines pages sinon
+     * Effectue une redirection du visiteur sur la page de connexion/creation de compte
+     */
+
        public static function isAuth(): void {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
@@ -17,6 +23,11 @@ class Auth {
             exit();
         }
     }
+
+    /**
+     * Deconnecte l'utilisateur en supprimant la session
+     * 
+     */
 
     public static function offAuth() :void {
 
