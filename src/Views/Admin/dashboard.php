25ecb1179107118869494eb2ccef8f1512ca6dcf
BenjCaro <carbenjamin@gmail.com>
2025-09-04 13:14:20 +0200

    ajout titre page dashboard

--- src/Views/Admin/dashboard.php
+++ src/Views/Admin/dashboard.php
@@ -6,6 +6,9 @@ namespace Carbe\App\Views\Admin;
 ?>
 
 <main class='container p-3 bg-light'>
+    <section>
+      <h1>Bonjour Admin</h1>  
+    </section>
     <section class="row d-flex justify-content-center">
         <h3 class="text-center mt-4 mb-4">Derniers Utilisateurs</h3>
         <div class="table-responsive">
