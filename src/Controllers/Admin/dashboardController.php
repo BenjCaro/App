dd5aaa03c3b967b10ad893c871bb51d801736f9a
BenjCaro <carbenjamin@gmail.com>
2025-09-04 13:45:49 +0200

    decouverte d'un probleme de stockage de données en session

--- src/Controllers/Admin/dashboardController.php
+++ src/Controllers/Admin/dashboardController.php
@@ -2,16 +2,22 @@
 namespace Carbe\App\Controllers\Admin;
 
 use Carbe\App\Controllers\BaseController;
+use Carbe\App\Models\UserModel;
 use Carbe\App\Services\Auth;
 
 class DashboardController extends BaseController {
 
     public function index() :void {
         
-       // Auth::isAdmin();
-
+      Auth::isAdmin();
+        
+       $adminId = $_SESSION['auth_user']['id'];
+       $admin = new UserModel($this->pdo);
+       $admin->findById($adminId);
+        
         $this->render('dashboard',  [
             'title' => 'Petit Creux | Dashboard',
+            'admin' => $admin
         
       ]);
     }
--- src/Views/Admin/header.php
+++ src/Views/Admin/header.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace Carbe\App\Views\Admin;
-
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
 
 <!DOCTYPE html>
