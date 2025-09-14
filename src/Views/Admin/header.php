4a7556afd7057ac936343e160a5c9a4e98d7b64a
BenjCaro <carbenjamin@gmail.com>
2025-09-14 12:14:09 +0200

    ajustement menu du header du tableau de bord admin

--- src/Views/Admin/header.php
+++ src/Views/Admin/header.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Carbe\App\Views\Admin;
+use Carbe\App\Services\Auth;
 
 /** @var string $title */
 
@@ -21,20 +22,21 @@ if (session_status() === PHP_SESSION_NONE) {
     <header class="border-bottom  border-cacao border-2">
         <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid">
+                <a class="navbar-brand" href="/">
+                    <img src="/assets/images/Icone_Petit_Creux.svg" alt="Logo">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuPrincipal">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div>
-                    <a href="/admin" class="nav-link fs-5">Tableau de Bord</a>
-                </div>
-                <div class="collapse navbar-collapse" id="menuPrincipal">
+               <div class="collapse navbar-collapse" id="menuPrincipal">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item fs-5"><a class="nav-link" href="/admin">Tableau de Bord</a></li>
                         <li class="nav-item fs-5"><a class="nav-link" href="">Utilisateurs</a></li>
                         <li class="nav-item fs-5"><a class="nav-link" href="">Recettes</a></li>
                         <li class="nav-item fs-5"><a class="nav-link" href="">Commentaires</a></li>
                         <li class="nav-item fs-5"><a class="nav-link" href="">Catégories</a></li> 
                         <li class="nav-item fs-5"><a class="nav-link" href="">Ingrédients</a></li>
-                        <li class="nav-item fs-5"><a class="nav-link" href="">Déconnexion</a></li>      
+                        <li class="nav-item fs-5"><a class="nav-link" href="/logout">Déconnexion</a></li>   
                     </ul>
                 </div>
             </div>
