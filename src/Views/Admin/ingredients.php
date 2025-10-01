776e847c09a774424cbb22f89fe6d989a09ffa44
BenjCaro <carbenjamin@gmail.com>
2025-10-01 16:51:07 +0200

    utilisation des data attributs pour modifier dynamiquement les valerus des champs des form déclenchés par les boutons modifier et supprimer

--- src/Views/Admin/ingredients.php
+++ src/Views/Admin/ingredients.php
@@ -22,15 +22,15 @@ namespace Carbe\App\Views\Admin;
                     <?php foreach($ingredients as $ingredient) { ?>
                     <tr>
                         <td><?= htmlspecialchars($ingredient->getName()) ?></td>
-                        <td><?= htmlspecialchars($ingredient->getType()) ?></td>
-                        
+                        <td><?= htmlspecialchars($ingredient->getType()) ?></td> 
                         <td>
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#hiddenForm">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#hiddenForm" 
+                              data-id= "<?= $ingredient->getId() ?>" data-name="<?= htmlspecialchars($ingredient->getName()) ?>" data-type="<?= htmlspecialchars($ingredient->getType()) ?>">
                                Modifier
                             </button>
                         </td>
                         <td>
-                            <button class="btn btn-orange" id="deleteIngredient">
+                            <button class="btn btn-orange" data-bs-toggle="modal" data-bs-target="#hiddenDeleteForm" data-id= "<?= $ingredient->getId() ?>">
                                 Supprimer
                             </button>
                         </td>
@@ -42,26 +42,73 @@ namespace Carbe\App\Views\Admin;
         <div class="modal fade" id="hiddenForm" tabindex="-1" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
-                <div class="modal-header">
-                    <h2 class="modal-title">Modifier l'ingrédient</h2>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="" method="post">
-                    <input type="hidden" name="id" value="<?= $ingredient->getId() ?>">
-                    <div>
-                        <label for="">Nom</label>
-                        <input type="text" name="name">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Modifier l'ingrédient</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
                     </div>
-                    <div>
-                        <label for="">Type</label>
-                        <input type="text" name="type">
+                    <div class="modal-body">
+                        <form action="" method="post" id="updateForm">
+                            <input type="hidden"  name="id" value="">
+                            <div class="mb-3">
+                                <label for="ingredientName" class="form-label">Nom</label>
+                                <input type="text" id="ingredientName" name="name" class="form-control" value="">
+                            </div>
+                            <div class="mb-3">
+                                <label for="ingredientType" class="form-label">Type</label>
+                                <input type="text" id="ingredientType" name="type" class="form-control" value="">
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <button type="button" class="btn btn-secondary me-2" data-bs-dismiss="modal">Annuler</button>
+                                <button type="submit" class="btn btn-primary">Valider</button>
+                            </div>
+                        </form>
                     </div>
-                    <button type="submit" class="btn btn-success mt-3">Valider</button>
-                    </form>
                 </div>
+            </div>
+        </div>
+        <div class="modal fade" id="hiddenDeleteForm" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2>Supprimer l'ingrédient</h2>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="" method="POST" id="deleteForm">
+                            <input type="hidden" name="id" value="">
+                            <p>Es-tu sûr de vouloir supprimer cet ingrédient ?</p>
+                            <div class="mt-3 d-flex justify-content-end">
+                                <button type="button" class="btn btn-secondary me-2" data-bs-dismiss="modal">Annuler</button>
+                                <button type="submit" class="btn btn-primary">Supprimer</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </main>
+<script>
+    
+    const updateForm = document.getElementById("updateForm");
+
+    document.querySelectorAll('[data-bs-target="#hiddenForm"]').forEach(btn => {
+        btn.addEventListener('click', () => {
+            updateForm.querySelector('input[name="id"]').value = btn.dataset.id;
+            updateForm.querySelector('input[name="name"]').value = btn.dataset.name;
+            updateForm.querySelector('input[name="type"]').value = btn.dataset.type;
+            });
+    });
+
+    const deleteForm = document.getElementById("deleteForm");
+
+    document.querySelectorAll('[data-bs-target="#hiddenDeleteForm"]').forEach(btn => {
+        btn.addEventListener('click', () => {
+            deleteForm.querySelector('input[name="id"').value = btn.dataset.id;
+        });
+    });
+    
+            
+
+</script>
+
